c1bef216453df269958526e9c294f074712c6819
biswa <biswasmrutibaral2003@gmail.com>
2025-09-08 18:06:38 +0530

    add new  line of code

--- App.js
+++ App.js
@@ -3,3 +3,5 @@ let h1 = document.querySelector("h1");
 h1.addEventListener("click " ,()=>{
     document.querySelector("body").style.backgroundColor = "beige";
 })
+
+console.log("well come to my project2");
